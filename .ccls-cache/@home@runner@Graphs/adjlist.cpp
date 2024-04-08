5532dfa0c887f937ac93537890dce878b4dba729
Abhishek Sharma <113334317+AbhishekSaroch@users.noreply.github.com>
2024-04-08 09:16:21 +0000

    Graphs implementation using adjlist

---
+++ .ccls-cache/@home@runner@Graphs/adjlist.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Graph {
+public:
+  unordered_map<int, list<int>> adjList;
+
+  void addEdge(int u, int v, bool direction) {
+    // direction=0 -> undirected graph
+    // direction=1 -> directed graph
+    // create an edge from u to v
+    adjList[u].push_back(v);
+    if (direction == 0) {
+      // undirected graph
+      adjList[v].push_back(u);
+    }
+  }
+  void printAdjList() {
+    for (auto node : adjList) {
+      cout << node.first << "->";
+      for (auto neighbour : node.second) {
+        cout << neighbour << ",";
+      }
+      cout << endl;
+    }
+  }
+};
+
+int main() {
+  Graph g;
+  g.addEdge(0, 1, 0);
+  g.addEdge(1, 2, 0);
+  g.addEdge(0, 2, 0);
+
+  g.printAdjList();
+  return 0;
+}
